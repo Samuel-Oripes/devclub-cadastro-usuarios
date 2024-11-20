6cc384f5fd89a92a9c8f4ccac73f0ea7005cc3a1
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2024-11-20 17:00:41 -0300

    Aprendendo sobre React

---
+++ src/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+    color: red;
+`
+
+export const Container = styled.div`
+    background-color: green;
+
+    p{
+        color: yellow;
+        font-size: 20px;
+    }
+`
