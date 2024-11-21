843fd206a58b138286cf92a4f1f8d91dda52a7f2
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2024-11-21 17:13:58 -0300

    Avançando no React

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,49 @@
+import { useRef } from 'react'
+
+import { Title, Container, TopBackground, ContainerInputs, Input, Form, Button, InputLabel } from './styles'
+import UsersImage from '../../assets/users.png'
+
+function Home() {
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser(){
+
+  }
+
+  return (
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt='imagem-usuarios' />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <InputLabel>Nome <span>*</span>
+              <Input type='text' placeholder='Nome do usuário' ref={inputName}/>
+            </InputLabel>
+          </div>
+          <div>
+            <InputLabel>Idade <span>*</span>
+              <Input type='number' placeholder='Idade do usuário' ref={inputAge}/>
+            </InputLabel>
+          </div>
+        </ContainerInputs>
+        <div style={{ width: '100%' }}>
+          <InputLabel>E-mail <span>*</span>
+            <Input type='email' placeholder='E-mail do usuário' ref={inputEmail}/>
+          </InputLabel>
+        </div>
+        <Button type='button' onClick={registerNewUser}>Cadastrar Usuários</Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default Home
+
